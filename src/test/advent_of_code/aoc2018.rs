5e4edc3315940bb4cf0aa9f2c7f9652ebe4fb412
ben <ben@home.com>
2023-11-18 14:11:38 +1100

    aoc 2018 day 3

--- src/test/advent_of_code/aoc2018.rs
+++ src/test/advent_of_code/aoc2018.rs
@@ -1,8 +1,10 @@
+#[cfg(test)] use crate::rust_function_ctx::{RustFunctionCtx, RustValue};
+#[cfg(test)] use crate::test::utils;
+#[cfg(test)] use crate::vm;
+
 #[cfg(test)]
 mod day1 {
-    use crate::rust_function_ctx::{RustFunctionCtx, RustValue};
-    use crate::test::utils;
-    use crate::vm;
+    use super::*;
 
     fn input(ctx: &mut dyn RustFunctionCtx) {
         let mut v = vec![];
@@ -91,9 +93,7 @@ mod day1 {
 
 #[cfg(test)]
 mod day2 {
-    use crate::rust_function_ctx::{RustFunctionCtx, RustValue};
-    use crate::test::utils;
-    use crate::vm;
+    use super::*;
 
     fn input(ctx: &mut dyn RustFunctionCtx) {
         let mut v = vec![];
@@ -248,4 +248,141 @@ mod day2 {
         assert_eq!(printer.strings[1], "omlvgdokxfncvqyersasjwziup");
         assert_eq!(printer.strings[2], "omlvgdokxfncvqyersasjlziup");
     }
+
+}
+
+#[cfg(test)]
+mod day3 {
+    use super::*;
+
+    fn input(ctx: &mut dyn RustFunctionCtx) {
+        let mut v = vec![];
+
+        for l in include_str!("aoc2018/day3-input.txt").lines() {
+            v.push(l.to_string());
+        }
+
+        ctx.set_result(RustValue::StringVector(v));
+    }
+
+    #[test]
+    fn part_1_and_2() {
+        let mut printer = utils::TestPrinter::new();
+        let mut vm = vm::VM::new(&mut printer);
+
+        vm.create_function("fn readinput() -> [string]", &input)
+            .expect("Failed to create function");
+
+        let src = "
+            let lines: [string] = readinput();
+
+            let mut map: [int: [int: int]] = hash_map<int, [int: int]>{};
+
+            for i : 0..lines.len() {
+                let l: string = lines[i][1..];
+                let vid: [string] = l.split('@');
+                let vcoord: [string] = vid[1].split(':');
+
+                let id: string = vid[0].trim();
+                let scoord: string = vcoord[0].trim();
+                let coord: [string] = scoord.split(',');
+                let sarea: string = vcoord[1].trim();
+                let area: [string] = sarea.split('x');
+
+                let start_x: int = coord[0].to_integer();
+                let start_y: int = coord[1].to_integer();
+
+                let ax: int = area[0].to_integer();
+                let ay: int = area[1].to_integer();
+
+                let end_x: int = start_x + ax;
+                let end_y: int = start_y + ay;
+
+                for x : start_x..end_x {
+                    for y : start_y..end_y {
+                        if map.contains_key(x) {
+                        }
+                        else {
+                            map[x] = hash_map<int, int>{};
+                        }
+
+                        let mut mapx: [int: int] = map[x];
+                        if mapx.contains_key(y) {
+                            let count: int = mapx[y] + 1;
+                            mapx[y] = count;
+                        }
+                        else {
+                            mapx[y] = 1;
+                        }
+                    }
+                }
+            }
+
+            let mut overlapping_claims: int = 0;
+            let xkeys: [int] = map.keys();
+
+            for ix : 0..xkeys.len() {
+                let mapx: [int: int] = map[xkeys[ix]];
+                let ykeys: [int] = mapx.keys();
+
+                for iy : 0..ykeys.len() {
+                    if mapx[ykeys[iy]] > 1 {
+                        overlapping_claims = overlapping_claims + 1;
+                    }
+                }
+            }
+
+            // part 1
+            print(overlapping_claims);
+
+            union R {
+                Overlapped,
+                NotOverlapped(string),
+            }
+
+            fn is_overlapped(line: string) -> R {
+                let l: string = line[1..];
+                let vid: [string] = l.split('@');
+                let vcoord: [string] = vid[1].split(':');
+
+                let id: string = vid[0].trim();
+                let scoord: string = vcoord[0].trim();
+                let coord: [string] = scoord.split(',');
+                let sarea: string = vcoord[1].trim();
+                let area: [string] = sarea.split('x');
+
+                let start_x: int = coord[0].to_integer();
+                let start_y: int = coord[1].to_integer();
+
+                let ax: int = area[0].to_integer();
+                let ay: int = area[1].to_integer();
+
+                let end_x: int = start_x + ax;
+                let end_y: int = start_y + ay;
+
+                for x : start_x..end_x {
+                    for y : start_y..end_y {
+                        let mut mapx: [int: int] = map[x];
+                        if mapx[y] > 1 {
+                            return R.Overlapped;
+                        }
+                    }
+                }
+
+                return R.NotOverlapped(id,);
+            }
+
+            // part 2
+            for i : 0..lines.len() {
+                if let R.NotOverlapped(id,) = is_overlapped(lines[i]) {
+                    print(id);
+                }
+            }";
+
+        assert_eq!(vm.interpret(src), Ok(()));
+        assert_eq!(printer.strings.len(), 2);
+        assert_eq!(printer.strings[0], "113576");
+        assert_eq!(printer.strings[1], "825");
+    }
+
 }
