d8bffda9232dea50a7ddc4002505d79e673ba7f6
ben <ben@home.com>
2023-11-27 22:30:59 +1100

    aoc 2018 day7

--- src/test/advent_of_code/aoc2018.rs
+++ src/test/advent_of_code/aoc2018.rs
@@ -278,6 +278,37 @@ mod day6 {
 }
 
 #[cfg(test)]
+mod day7 {
+    use super::*;
+
+    fn input(ctx: &mut dyn RustFunctionCtx) {
+        let mut v = vec![];
+
+        for l in include_str!("aoc2018/day7-input.txt").lines() {
+            v.push(l.to_string());
+        }
+
+        ctx.set_result(RustValue::StringVector(v));
+    }
+
+    #[test]
+    fn part_1_and_2() {
+        let mut printer = utils::TestPrinter::new();
+        let mut vm = vm::VM::new(&mut printer);
+
+        vm.create_function("fn readinput() -> [string]", &input)
+            .expect("Failed to create function");
+
+        let src = include_str!("aoc2018/day7.kyx");
+
+        assert_eq!(vm.interpret(src), Ok(()));
+        assert_eq!(printer.strings.len(), 27);
+        assert_eq!(printer.strings[0..26].concat(), "ABDCJLFMNVQWHIRKTEUXOZSYPG");
+        assert_eq!(printer.strings[26], "896");
+    }
+}
+
+#[cfg(test)]
 mod day8 {
     use super::*;
 
