201c28ec14996c30ce97c44fe02f0f19954ecbae
ben <ben@home.com>
2023-12-20 21:33:48 +1100

    aoc2023 day12

--- src/test/advent_of_code/aoc2023.rs
+++ src/test/advent_of_code/aoc2023.rs
@@ -314,3 +314,34 @@ mod day11 {
         assert_eq!(printer.strings[1], "569052586852");
     }
 }
+
+#[cfg(test)]
+mod day12 {
+    use super::*;
+
+    fn input(ctx: &mut dyn RustFunctionCtx) {
+        let mut v = vec![];
+
+        for l in include_str!("aoc2023/day12-input.txt").lines() {
+            v.push(l.to_string());
+        }
+
+        ctx.set_result(RustValue::StringVector(v));
+    }
+
+    #[test]
+    fn part1_and_2() {
+        let mut printer = utils::TestPrinter::new();
+        let mut vm = vm::VM::new(&mut printer);
+
+        vm.create_function("fn readinput() -> [string]", &input)
+            .expect("Failed to create function");
+
+        let src = include_str!("aoc2023/day12.kyx");
+
+        assert_eq!(vm.interpret(src), Ok(()));
+        assert_eq!(printer.strings.len(), 2);
+        assert_eq!(printer.strings[0], "6852");
+        assert_eq!(printer.strings[1], "8475948826693");
+    }
+}
